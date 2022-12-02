80958734101a972c9c309df88217996a4917a6d9
SashAwl <aleks.shilova@gmail.com>
2022-12-02 15:10:51 +0300

    hw_5: added comments to the code

--- Editor_3D.java
+++ Editor_3D.java
@@ -1,4 +1,5 @@
 import Enumerators.ColorSchema;
+import Enumerators.OtherTypeModel;
 
 
 public enum ColorSchema {
@@ -18,6 +19,9 @@ public enum OtherTypeModel {
     IFC
 }
 
+/*
+ * Model - сущность 3D-модели с некоторыми атрибутами
+ */
 public class Model {
     private Integer countPoligons;
     private Integer countPicks;
@@ -26,6 +30,9 @@ public class Model {
     private boolean texture;
 }
 
+/*
+ * Rendering - сущность, полученная путем применения метода renderModel(Model model) 
+ */
 public class Rendering {
     private Integer resolutionOx;
     private Integer resolutionOy;
@@ -33,16 +40,23 @@ public class Rendering {
     private ColorSchema colorSchema;
 
 }
-
+/*
+ * Editor - редактор, позволяющий вносить изменения в переданную(только созданную или загруженную) модель 
+ * *посредством применения метода editModel(Model model)
+ */
 public class Editor {
     public Model editModel(Model model) {
         return model;
     }
 }
 
+/* iRepo - интерфейс, позволяющий предотвратить зависимость более абстрактного класса BusinessLogic 
+**от менее абстрактного класса Repository 
+*/
 public interface iRepo {
     public Model getModel();
 }
+
 
 public class Repository implements iRepo{
     public Model getModel(){
@@ -89,5 +103,27 @@ public class View  implements iView {
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+    }
+}
+
+public class BusinessLogic{
+    public Model convertationModel(OtherTypeModel otherModel){
+        new BusinessLogic().changeFormat(otherModel);
+        new Repository().saveChanges();
+        return new Model();
+    }
+
+    private Model changeFormat(OtherTypeModel otherModel){
+        return new Model();
+    }
+
+    public Model createModel(){
+        iView iview = new View();
+        iview.selectNewModel();
+        return new Model();
+    }
+
+    public Rendering renderModel(Model model) {
+        return new Rendering();
     }
 }
