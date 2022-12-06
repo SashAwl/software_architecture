fd37df4bdfbe7fcb023e1a05f2ce234e4b28af98
SashAwl <aleks.shilova@gmail.com>
2022-12-06 13:40:49 +0300

    hw_7: added interface iSend

--- hw_7.java
+++ hw_7.java
@@ -1,3 +1,15 @@
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+
+/**
+ * PartsRequest - сущность, содержащая детали полученного HTTP-запроса
+*/
 class PartsRequest{
     public String method;
     public String URL;
@@ -6,15 +18,26 @@ class PartsRequest{
     public String bodyHTTP;
 }
 
+/**
+ * ReportObject - сущность сформированного отчета в исходном формате
+ */
 class ReportObject{
-    public Date dataObject;
+    public Date dateObject;
     public Integer volumObjectKb;
 }
 
+/**
+ * JSON - сущность отчета, представленного в подготовленном для отправки формате
+ */
 class JSON{
     public Integer length;
 }
 
+/**
+ * Sender - класс, содержащий методы для отправки данных в приложение
+ * convertJSON(ReportObject someReport) - конвертирует отчет из исходного формата в формат JSON
+ * send(JSON reportJSON) - осуществляет непосредственно отправку подготовленного отчета
+ */
 class Sender{
     public JSON convertJSON(ReportObject someReport){
         return new JSON();
@@ -25,55 +48,89 @@ class Sender{
     }
 }
 
+/**
+ * iSend - интерфейс, позволяющий исключить зависимость более абстрактного класса Report
+ * * от менее абстрактного класса Sender 
+ */
+interface iSend{
+    public void send(JSON reportJSON);
+}
+
+/**
+ * Report - класс, содержащий методы для формирования отчета
+ * getData(PartsRequest detailsRequest) - запрашивает данные для формирования отчета у источника данных
+ * transfomRequest(String request) - разбивает на содержательные части полученный HTTP-запрос
+ * bildReport(ArrayList<Sales> list) - строит отчет по полученным с помощью метода getData() данным
+ * sendReport(ReportObject someReport) - обращается к методам класса Sender для конвертации и отправки отчета
+ */
 class Report{
     public ArrayList<Sales> getData(PartsRequest detailsRequest){
-        return new iSales().getSales();
+        iSales isales = new Provider();
+        return isales.getSales();
     }
 
     public PartsRequest transfomRequest(String request){
         return new PartsRequest();
     }
 
-    public bildReport(ArrayList<Sales> list){
-        System.out.println("Report successfully built!")
+    public ReportObject bildReport(ArrayList<Sales> list){
+        return new ReportObject();
     }
 
     public void sendReport(ReportObject someReport){
-        JSON reportJSON = new Sender.convertJSON(someReport);
-        reportJSON.send();
+        iSend isend = new Sender();
+        JSON reportJSON = isend.convertJSON(someReport);
+        isend.send(reportJSON);
     }
 }
 
+/**
+ * Reader - класс, отвечающий за инициализацию процесса сбора данных и формирования отчета
+ * * по полученному HTTP-запросу
+ * атрибут request содержит сам HTTP-запрос
+ * getReport() - отвечает за всю цепочку методов по формированию отчета
+ */
 class Reader{
-    public String request;
+    public static String request;
 
     public void getReport(){
         Report newReport = new Report();
         PartsRequest parts = newReport.transfomRequest(Reader.request);
         ArrayList<Sales> listSales = newReport.getData(parts);
-        return newReport.bildReport(listSales);
+        ReportObject someReport = newReport.bildReport(listSales);
+        newReport.sendReport(someReport);
     }
 }
 
+/**
+ * Sales - сущность, содержащая информацию об одном факте продажи товара
+ */
 class Sales{
     public String product;
     public String provider;
-    public Decimal purchase;
+    public Float purchase;
     public Integer countProduct;
-    public DateTime dateTimeOperation;
+    public Date dateTimeOperation;
     public Integer transactionNumber;
     public String salesman;
     public String adressShop;
     public String calculationMethod;
 }
 
+/**
+ * iSales - интерфейс, позволяющий исключить зависимость более абстрактного класса Report
+ * * от менее абстрактного класса Provider 
+ */
 interface iSales{
     public ArrayList<Sales> getSales();
 }
 
-class Provider implementation iSales{
+/**
+ * Provider - класс, отвечающий за подготовку данных из источника данных; реализует интерфейс iSales
+ * getSales() - возвращает список продаж
+ */
+class Provider implements iSales{
     public ArrayList<Sales> getSales(){
-       return new ArrayList<Sales>
+        return new ArrayList<Sales>();
     }
 }
-
